c5034c18d90bac2239c0bff00995b43084c12e98
DevJimmy <108478695+devJimmy990@users.noreply.github.com>
2025-03-02 17:59:43 +0200

    apply theme feature

---
+++ lib/core/theme/theme_data.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart' as material;
+
+class ThemeData {
+  ThemeData.dark() {
+    material.ThemeData.dark();
+  }
+  ThemeData.light() {
+    material.ThemeData.light();
+  }
+}
