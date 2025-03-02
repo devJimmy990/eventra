87473515d6d7a6771935fb0e7c2a8bfb09895070
DevJimmy <108478695+devJimmy990@users.noreply.github.com>
2025-03-02 17:46:25 +0200

    use go router manage routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:eventra/generated/l10n.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:eventra/core/routes/go_router.dart';
 import 'package:eventra/core/firebase_options.dart';
 import 'package:eventra/core/shared_preference.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -17,20 +18,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       localizationsDelegates: [
         S.delegate,
-        GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: S.delegate.supportedLocales,
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello, World!'),
-        ),
-      ),
+      routerConfig: router,
     );
   }
 }
