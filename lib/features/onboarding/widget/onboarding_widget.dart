84d816cc34eb4712d30bb7a3ec6a46f3b81ec650
Ahmed Roshdy <ahmedroshdy654@gmail.com>
2025-03-03 21:37:35 +0200

    commit onboarding_widget

--- lib/features/onboarding/widget/onboarding_widget.dart
+++ lib/features/onboarding/widget/onboarding_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 
 PageViewModel onBoarding({
@@ -9,42 +10,70 @@ PageViewModel onBoarding({
   double paddingTop = 100,
 }) {
   return PageViewModel(
-    title: title,
-    body: body,
+    titleWidget: const SizedBox(),
+    bodyWidget: Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.w),
+          child: Text(
+            title,
+            style: TextStyle(
+              fontSize: 24.sp,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        SizedBox(height: 10.h),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.w),
+          child: Text(
+            body,
+            style: TextStyle(
+              fontSize: 16.sp,
+              color: Colors.grey,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ],
+    ),
     image: Padding(
-      padding: EdgeInsets.only(top: paddingTop),
+      padding: EdgeInsets.only(top: paddingTop.h),
       child: Center(
         child: Image.asset(
           imagePath,
-          height: imageHeight,
+          height: imageHeight.h,
         ),
       ),
     ),
   );
 }
-
 final List<PageViewModel> onboardingModel = [
   onBoarding(
     title: 'Welcome',
     body: 'Discover the app that makes your life easier',
     imagePath: 'assets/images/on_boarding_images/onboarding1.gif',
-    paddingTop: 100,
+
   ),
   onBoarding(
     title: 'Feature Highlight',
     body: 'Explore our exciting features',
     imagePath: 'assets/images/on_boarding_images/onboardingC.gif',
+
   ),
   onBoarding(
     title: 'Stay Connected',
     body: 'Stay connected with friends and family',
     imagePath: 'assets/images/on_boarding_images/onboarding1.gif',
-    paddingTop: 120,
+
   ),
   onBoarding(
     title: 'Get Started',
     body: 'Let’s get you started!',
     imagePath: 'assets/images/on_boarding_images/onboarding1.gif',
-    paddingTop: 140,
+
   ),
 ];
