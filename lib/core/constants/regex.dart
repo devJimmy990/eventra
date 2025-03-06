cd6ae7b810b8835e9c2946656f2e5a0262faf1d1
DevJimmy <108478695+devJimmy990@users.noreply.github.com>
2025-03-06 15:49:33 +0200

    feat: update regex, string manager classes

--- lib/core/constants/regex.dart
+++ lib/core/constants/regex.dart
@@ -1,3 +1,7 @@
-bool emailValid (String email)=>
-    RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-        .hasMatch(email);
+class Regex {
+  static bool matchEmail(String value) =>
+      RegExp(r"^[a-zA-Z0-9_-]+@([a-zA-Z]+\.)+[a-zA-Z]{2,}$").hasMatch(value);
+
+  static bool matchPhone(String value) =>
+      RegExp(r"^01[0125]\d{8}$").hasMatch(value);
+}
--- lib/core/constants/strings_manager.dart
+++ lib/core/constants/strings_manager.dart
@@ -1,8 +1,10 @@
 class StringsManager {
-
   // Animations
-static const String loadingJson = "assets/animations/Animation - 1740842222236.json";
+  static const String loadingJson =
+      "assets/animations/Animation - 1740842222236.json";
 
 // Event Default
-static const String eventImage="assets/images/event.jpg";
+  static const String eventImage = "assets/images/event.jpg";
+  static const String appImage = 'assets/images/eventra.png';
+  static const String googleImage = 'assets/images/google.png';
 }
