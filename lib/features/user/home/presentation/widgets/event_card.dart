3911930472be7dfb7da0e058a0f1a52b1e17ae5b
DevJimmy <108478695+devJimmy990@users.noreply.github.com>
2025-03-11 00:49:53 +0200

    update event card

--- lib/features/user/home/presentation/widgets/event_card.dart
+++ lib/features/user/home/presentation/widgets/event_card.dart
@@ -1,72 +1,115 @@
 import 'package:eventra/core/constants/strings_manager.dart';
+import 'package:eventra/features/admin/event/extension/date_time.dart';
 import 'package:eventra/features/admin/event/model/base_event.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class EventCard extends StatelessWidget {
-  EventCard({super.key, required this.event});
-  BaseEvent? event;
+  final BaseEvent event;
+  const EventCard({super.key, required this.event});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: REdgeInsets.all(10.0),
-      child: Container(
-        width: 200.w,
-        margin: REdgeInsets.only(right: 10),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(12.r),
-          boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 5.r)],
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.vertical(top: Radius.circular(12.r)),
-              child: Image.asset(event?.cover ?? StringsManager.eventImage,
-                  height: 120.h, width: double.infinity, fit: BoxFit.cover),
-            ),
-            Padding(
-              padding: REdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(event?.title ?? "International Band Music Concert",
-                      style: TextStyle(shadows: [
-                        Shadow(
-                          blurRadius: 3.r,
-                        ),
-                      ], fontWeight: FontWeight.bold, fontSize: 20.sp)),
-                  SizedBox(height: 5.h),
-                  Text(
-                    event?.schedule.date.day.toString() ?? "14 December, 2021",
-                    style: TextStyle(
-                      shadows: [
-                        Shadow(
-                          blurRadius: 3.r,
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 5.h),
-                  Text(
-                    event?.location.address ??
-                        "5 Tawfik Diab Street, Garden City",
-                    style: TextStyle(
-                      color: Colors.grey,
-                      shadows: [
-                        Shadow(
-                          blurRadius: 3.r,
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+    return Card(
+      margin: REdgeInsets.only(right: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.vertical(top: Radius.circular(12.r)),
+                child: event.cover != null
+                    ? Image.asset(
+                        StringsManager.eventImage,
+                        fit: BoxFit.cover,
+                        height: 200.h,
+                        width: double.infinity,
+                      )
+                    : Image.network(
+                        event.cover!,
+                        fit: BoxFit.cover,
+                        height: 200.h,
+                        width: double.infinity,
+                      ),
               ),
+              Padding(
+                padding: EdgeInsets.all(8.r),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(211, 255, 255, 255),
+                        borderRadius: BorderRadius.all(Radius.circular(12.r)),
+                      ),
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(
+                        spacing: 0,
+                        children: [
+                          Text(
+                            event.schedule.date.day.toString(),
+                            style: TextStyle(
+                              fontSize: 18,
+                              shadows: [
+                                Shadow(
+                                  blurRadius: 3.r,
+                                ),
+                              ],
+                            ),
+                          ),
+                          Text(
+                            event.schedule.date.encodeMonth(),
+                            style: TextStyle(
+                              fontSize: 12,
+                              shadows: [
+                                Shadow(
+                                  blurRadius: 3.r,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    CircleAvatar(
+                      child: Icon(
+                        Icons.bookmark_border_outlined,
+                        size: 24,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: REdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(event.title,
+                    style: TextStyle(shadows: [
+                      Shadow(
+                        blurRadius: 3.r,
+                      ),
+                    ], fontWeight: FontWeight.bold, fontSize: 20.sp)),
+                SizedBox(height: 5.h),
+                Text(
+                  event.location.address,
+                  style: TextStyle(
+                    color: Colors.grey,
+                    shadows: [
+                      Shadow(
+                        blurRadius: 3.r,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
