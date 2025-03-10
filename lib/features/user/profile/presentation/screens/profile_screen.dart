66e0e211e3ee9073f8259b12f629c0e89554fe90
DevJimmy <108478695+devJimmy990@users.noreply.github.com>
2025-03-10 05:22:14 +0200

    update profile tabs

--- lib/features/user/profile/presentation/screens/profile_screen.dart
+++ lib/features/user/profile/presentation/screens/profile_screen.dart
@@ -1,141 +1,59 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:flutter_bloc/flutter_bloc.dart'
-;import 'package:eventra/core/routes/routes.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:eventra/features/user/profile/cubit/profile_cubit.dart';
-import 'package:eventra/features/user/profile/presentation/widgets/profile_tabs.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:eventra/features/landing/data/model/user.dart';
+import 'package:eventra/features/landing/cubit/user_cubit.dart';
+import 'package:eventra/features/user/profile/presentation/widgets/profile_events_tab.dart';
 
-
-class MyProfilePage extends StatefulWidget {
-  const MyProfilePage({Key? key}) : super(key: key);
-
-  @override
-  _MyProfilePageState createState() => _MyProfilePageState();
-}
-
-class _MyProfilePageState extends State<MyProfilePage>
-    with SingleTickerProviderStateMixin {
-  late final TabController tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    tabController = TabController(length: 2, vsync: this);
-    final User? user = FirebaseAuth.instance.currentUser;
-    if (user != null) {
-      context.read<ProfileCubit>().fetchUserProfile(user.uid);
-    }
-  }
-
-  @override
-  void dispose() {
-    tabController.dispose();
-    super.dispose();
-  }
+class MyProfilePage extends StatelessWidget {
+  const MyProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    final User user = context.read<UserCubit>().user!;
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 2,
+      child: Scaffold(
         backgroundColor: Colors.white,
-        body: BlocConsumer<ProfileCubit, ProfileState>(
-          listener: (context, state) {
-            if (state is ProfileError) {
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(content: Text(state.message)),
-              );
-            }
-          },
-          builder: (context, state) {
-            if (state is ProfileLoading) {
-              return Center(child: CircularProgressIndicator());
-            } else if (state is ProfileLoaded) {
-              final userData = state.userData;
-              final name = userData['name'] ?? 'No Name';
-              final avatarPath = userData['avatar'] ?? 'assets/images/user.png';
-              final phone = userData['phone'] ?? 'No phone number available.';
-              final email = userData['email'] ?? 'No email available.';
-              final aboutMe = userData['bio'] ?? 'No bio available.';
-
-              return Center(
-                child: Column(
-                  children: [
-                    SizedBox(
-                        height:
-                            MediaQuery.of(context).size.height * 0.02),
-                    CircleAvatar(
-                      radius: 50,
-                      backgroundImage: avatarPath.startsWith('assets/')
-                          ? AssetImage(avatarPath) as ImageProvider
-                          : FileImage(File(avatarPath)),
-                    ),
-                    SizedBox(
-                        height:
-                            MediaQuery.of(context).size.height * 0.02),
-                    Text(
-                      name,
-                      style: TextStyle(
-                        fontSize: 24,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(
-                        height:
-                            MediaQuery.of(context).size.height * 0.01),
-                    Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          TextButton.icon(
-                            onPressed: () {
-                              final Map<String, dynamic> args = {
-                                'currentName': name,
-                                'currentAvatarPath': avatarPath,
-                                'currentAboutMe': aboutMe,
-                                'currentPhone': phone,
-                                'currentEmail': email,
-                              };
-                            
-                              context.pushNamed(
-                                UserRoutes.editProfile,
-                                extra: args,
-                              );
-                            },
-                            icon: Icon(
-                              Icons.edit_sharp,
-                              color: Colors.deepPurpleAccent,
-                            ),
-                            label: Text(
-                              'Edit Profile',
-                              style: TextStyle(
-                                  color: Colors.deepPurpleAccent),
-                            ),
-                            style: TextButton.styleFrom(
-                              side: BorderSide(
-                                  color: Colors.deepPurpleAccent),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(12),
-                              ),
-                              padding: EdgeInsets.symmetric(
-                                  horizontal: 20, vertical: 12),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    ProfileTabs(tabController: tabController),
-                  ],
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: NetworkImage(user.avatar ?? ""),
                 ),
-              );
-            } else if (state is ProfileError) {
-              return Center(child: Text(state.message));
-            } else {
-              return Center(child: Text('No data available.'));
-            }
-          },
-        ));
+                SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+                Text(
+                  user.name,
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+                TabBar(tabs: [
+                  Tab(text: 'About'),
+                  Tab(text: 'Events'),
+                ]),
+                Expanded(
+                  child: TabBarView(
+                    children: [
+                      ProfileAboutTab(),
+                      Center(
+                        child: Text('Events'),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
