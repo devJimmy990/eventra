f0e80dc24bd9f4bb43cd24e9d00239bd72f1a02b
DevJimmy <108478695+devJimmy990@users.noreply.github.com>
2025-03-11 02:32:03 +0200

    update calendar screen and data-source

--- lib/features/user/home/presentation/view/calender_view.dart
+++ lib/features/user/home/presentation/view/calender_view.dart
@@ -1,19 +1,17 @@
 import 'package:eventra/core/routes/routes.dart';
 import 'package:eventra/features/admin/event/extension/event.dart';
-import 'package:eventra/features/user/calender/calender_data_source.dart';
 import 'package:eventra/features/user/home/cubit/event_cubit.dart';
 import 'package:eventra/features/user/home/cubit/event_state.dart';
+import 'package:eventra/features/user/home/controller/calender_data_source.dart';
 
-import 'package:eventra/features/user/home/data/model/booked_event.dart'; // for UserEvent
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:eventra/features/user/home/data/model/booked_event.dart';
 
-
-
-class EventCalendarPage extends StatelessWidget {
-  const EventCalendarPage({super.key});
+class EventCalendarView extends StatelessWidget {
+  const EventCalendarView({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +19,7 @@ class EventCalendarPage extends StatelessWidget {
       appBar: AppBar(title: const Center(child: Text("Event Calendar"))),
       body: BlocBuilder<UserEventCubit, UserEventState>(
         builder: (context, state) {
-           if (state is EventLoading) {
+          if (state is EventLoading) {
             return const Center(child: CircularProgressIndicator());
           } else if (state is EventError) {
             return Center(child: Text("Error: ${state.error}"));
@@ -36,7 +34,6 @@ class EventCalendarPage extends StatelessWidget {
               appointmentBuilder:
                   (BuildContext context, CalendarAppointmentDetails details) {
                 final event = details.appointments.first as UserEvent;
-                // Determine the index of the event to fetch its color.
                 final index = dataSource.appointments!.indexOf(event);
                 final tileColor = dataSource.getColor(index);
 
@@ -53,9 +50,8 @@ class EventCalendarPage extends StatelessWidget {
                   subtitle: Text(event.encodeLongTime(),
                       style: TextStyle(color: Colors.white)),
                   tileColor: tileColor,
-                  onTap: () {
-                    context.pushNamed(UserRoutes.home);
-                  },
+                  onTap: () =>
+                      context.pushNamed(UserRoutes.event, extra: event),
                 );
               },
             );
