c02ba21cbd2e1427b9595bcd00436d7a01a68ad3
DevJimmy <108478695+devJimmy990@users.noreply.github.com>
2025-03-12 05:19:38 +0200

    update user event screen

--- lib/features/user/home/presentation/screens/user_event_details_screen.dart
+++ lib/features/user/home/presentation/screens/user_event_details_screen.dart
@@ -1,16 +1,8 @@
 import 'dart:convert';
 import 'package:eventra/core/helper/localization.dart';
+import 'package:eventra/features/user/home/cubit/event_cubit.dart';
 import 'package:flutter/material.dart';
-import 'package:qr_flutter/qr_flutter.dart';
-import 'package:eventra/core/helper/external_launcher.dart';
-import 'package:eventra/core/constants/strings_manager.dart';
-import 'package:eventra/features/admin/event/extension/event.dart';
-import 'package:eventra/features/admin/event/extension/date_time.dart';
-import 'package:eventra/features/user/home/data/model/booked_event.dart';
-
-import 'dart:convert';
-import 'package:eventra/core/helper/localization.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:eventra/core/helper/external_launcher.dart';
 import 'package:eventra/core/constants/strings_manager.dart';
@@ -30,32 +22,39 @@ class UserEventDetailsScreen extends StatelessWidget {
       extendBodyBehindAppBar: true,
       body: CustomScrollView(
         slivers: [
-          SliverAppBar(iconTheme: IconThemeData(color: Colors.white),
+          SliverAppBar(
+            iconTheme: IconThemeData(color: Colors.white),
             backgroundColor: Colors.transparent,
             expandedHeight: 250,
             pinned: true,
             // Display the event cover image in the flexible space.
             flexibleSpace: FlexibleSpaceBar(
-              title: Text(strings.eventDetails, style: TextStyle(color: Colors.white),),
+              title: Text(
+                strings.eventDetails,
+                style: TextStyle(color: Colors.white),
+              ),
               background: event.cover == null
                   ? Image.asset(
-                StringsManager.eventImage,
-                fit: BoxFit.cover,
-              )
+                      StringsManager.eventImage,
+                      fit: BoxFit.cover,
+                    )
                   : Image.network(
-                event.cover!,
-                fit: BoxFit.cover,
-                errorBuilder: (context, error, stackTrace) {
-                  return Image.asset(
-                    StringsManager.eventImage,
-                    fit: BoxFit.cover,
-                  );
-                },
-              ),
+                      event.cover!,
+                      fit: BoxFit.cover,
+                      errorBuilder: (context, error, stackTrace) {
+                        return Image.asset(
+                          StringsManager.eventImage,
+                          fit: BoxFit.cover,
+                        );
+                      },
+                    ),
             ),
             actions: [
               IconButton(
-                icon: const Icon(Icons.bookmark, color: Colors.white,),
+                icon: const Icon(
+                  Icons.bookmark,
+                  color: Colors.white,
+                ),
                 tooltip: strings.bookmark,
                 onPressed: () {},
               ),
@@ -147,7 +146,8 @@ class UserEventDetailsScreen extends StatelessWidget {
                       version: QrVersions.auto,
                       size: 320,
                       gapless: false,
-                      embeddedImage: const AssetImage('assets/images/my_embedded_image.png'),
+                      embeddedImage: const AssetImage(
+                          'assets/images/my_embedded_image.png'),
                       embeddedImageStyle: QrEmbeddedImageStyle(
                         size: const Size(80, 80),
                       ),
@@ -176,7 +176,9 @@ class UserEventDetailsScreen extends StatelessWidget {
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(16.0),
         child: ElevatedButton(
-          onPressed: () {},
+          onPressed: () {
+            context.read<UserEventCubit>().bookEvent(event);
+          },
           child: Text(
             event.price == 0
                 ? strings.bookNow
@@ -187,171 +189,3 @@ class UserEventDetailsScreen extends StatelessWidget {
     );
   }
 }
-
-//
-// class UserEventDetailsScreen extends StatelessWidget {
-//
-//   final UserEvent event;
-//   const UserEventDetailsScreen({super.key, required this.event});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     final strings = Localization(context);
-//     return Scaffold(
-//       extendBodyBehindAppBar: true,
-//       appBar: AppBar(
-//         title: Text(strings.eventDetails,),
-//         actions: [
-//           IconButton(
-//             icon: const Icon(Icons.bookmark),
-//             tooltip: strings.bookmark,
-//             onPressed: () {},
-//           ),
-//         ],
-//       ),
-//       body: Column(
-//         children: [
-//           Expanded(
-//             child: SingleChildScrollView(
-//               child: Column(
-//                 children: [
-//                   event.cover == null
-//                       ? Image.asset(
-//                           StringsManager.eventImage,
-//                           width: double.infinity,
-//                           height: 200,
-//                           fit: BoxFit.cover,
-//                         )
-//                       : Image.network(
-//                           event.cover!,
-//                           width: double.infinity,
-//                           height: 200,
-//                           fit: BoxFit.cover,
-//                           errorBuilder: (context, error, stackTrace) {
-//                             return Image.asset(
-//                               StringsManager.eventImage,
-//                               width: double.infinity,
-//                               height: 200,
-//                               fit: BoxFit.cover,
-//                             );
-//                           },
-//                         ),
-//
-//                   Padding(
-//                     padding: const EdgeInsets.all(16.0),
-//                     child: Text(
-//                       event.title,
-//                       textAlign: TextAlign.center,
-//                       style: const TextStyle(
-//                         fontWeight: FontWeight.bold,
-//                         fontSize: 32,
-//                       ),
-//                     ),
-//                   ),
-//
-//                   ListTile(
-//                     leading: Card(
-//                       margin: EdgeInsets.zero,
-//                       color: const Color(0xFFECEEFD),
-//                       child: const CircleAvatar(
-//                         radius: 25,
-//                         backgroundColor: Colors.transparent,
-//                         child: Icon(
-//                           color: Color(0xFF5669FD),
-//                           Icons.calendar_month_outlined,
-//                           size: 30,
-//                         ),
-//                       ),
-//                     ),
-//                     title: Text(event.schedule.date.encodeDate()),
-//                     subtitle: Text(event.encodeLongDateTime()),
-//                   ),
-//
-//                   ListTile(
-//                     onTap: () => ExternalLauncher.launchLocation(
-//                       location: event.location.url,
-//                     ),
-//                     leading: Card(
-//                       margin: EdgeInsets.zero,
-//                       color: const Color(0xFFECEEFD),
-//                       child: const CircleAvatar(
-//                         radius: 25,
-//                         backgroundColor: Colors.transparent,
-//                         child: Icon(
-//                           color: Color(0xFF5669FD),
-//                           Icons.location_on,
-//                           size: 30,
-//                         ),
-//                       ),
-//                     ),
-//                     title: Text(event.location.name),
-//                     subtitle: Text(event.location.address),
-//                   ),
-//
-//                   const SizedBox(height: 16),
-//
-//                   // About Event Section
-//                   Padding(
-//                     padding: const EdgeInsets.symmetric(horizontal: 16),
-//                     child: Text(
-//                       strings.aboutEvent,
-//                       style: const TextStyle(
-//                         fontWeight: FontWeight.bold,
-//                         fontSize: 20,
-//                       ),
-//                     ),
-//                   ),
-//                   Padding(
-//                     padding: const EdgeInsets.symmetric(horizontal: 16),
-//                     child: Text(
-//                       event.desc,
-//                       textAlign: TextAlign.justify,
-//                     ),
-//                   ),
-//                   QrImageView(
-//                     data: event.toString(),
-//                     version: QrVersions.auto,
-//                     size: 320,
-//                     gapless: false,
-//                     embeddedImage:
-//                         AssetImage('assets/images/my_embedded_image.png'),
-//                     embeddedImageStyle: QrEmbeddedImageStyle(
-//                       size: Size(80, 80),
-//                     ),
-//                   ),
-//                   QrImageView(
-//                     data: json.encode(event),
-//                     version: QrVersions.auto,
-//                     size: 200.0,
-//                     backgroundColor: Colors.white,
-//                     eyeStyle: QrEyeStyle(
-//                       eyeShape: QrEyeShape.square,
-//                       color: Colors.black,
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           ),
-//
-//           // Book/Buy Ticket Button
-//           Padding(
-//             padding: const EdgeInsets.all(16.0),
-//             child: SizedBox(
-//               width: double.infinity,
-//               child: ElevatedButton(
-//                 onPressed: () {},
-//                 child: Text(
-//                   event.price == 0
-//                       ? "${strings.bookNow}"
-//                       : "${strings.buyTicket} \$${event.price}",
-//                 ),
-//
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
